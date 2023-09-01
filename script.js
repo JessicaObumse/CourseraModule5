52ef1060508bf0468a40f3d783650b8a3f719bd3
Jessica Obumse <140408092+JessicaObumse@users.noreply.github.com>
2023-09-01 14:35:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript code for handling interactions goes here
+
+// Example: Changing content of the Specials tile on click
+const specialsTile = document.getElementById("specials-tile");
+
+specialsTile.addEventListener("click", function () {
+    // Replace this with your logic to load random category content
+    specialsTile.innerHTML = "<h2>Random Category</h2><p>Specials content goes here.</p>";
+});
